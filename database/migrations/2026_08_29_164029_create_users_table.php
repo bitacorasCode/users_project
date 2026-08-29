c591531c4d4cbc08ba5fe321f216d45ef5713386
Ignacio Nuñez <ignacio.nd37@gmail.com>
2026-08-29 14:24:17 -0400

    wip: create user data tables and clean unused code and files

---
+++ database/migrations/2026_08_29_164029_create_users_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name', 100);
+            $table->string('last_name', 100);
+
+            $table->string('email')->unique();
+            $table->string('rut')->unique();
+
+            $table->string('phone')->nullable()->unique();
+
+            $table->foreignId('role_id')
+                ->constrained('roles')
+                ->restrictOnDelete();
+
+            $table->string('state');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
