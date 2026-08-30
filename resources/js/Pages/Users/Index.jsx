ca2d6ace2fe0cc1ce98f004feb444044327258b1
Ignacio Nuñez <ignacio.nd37@gmail.com>
2026-08-30 12:26:37 -0400

    feature: configure and install inertia and react

---
+++ resources/js/Pages/Users/Index.jsx
@@ -0,0 +1,17 @@
+import { Head } from "@inertiajs/react";
+
+export default function Index({ users }) {
+    return (
+        <>
+            <Head title="Usuarios" />
+
+            <h1>Usuarios</h1>
+
+            {users.map((user) => (
+                <div key={user.id}>
+                    {user.name} {user.last_name}
+                </div>
+            ))}
+        </>
+    );
+}
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,19 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/users', [UserController::class, 'index'])
+    ->name('users.index');
 
-// poner rutas inertia aca
+Route::get('/users/{user}/address', [UserController::class, 'address'])
+    ->name('users.address');
+
+Route::get('/users/{user}/notes', [UserController::class, 'notes'])
+    ->name('users.notes');
+
+Route::post('/users', [UserController::class, 'store'])
+    ->name('users.store');
+
+Route::delete('/users/{user}', [UserController::class, 'destroy'])
+    ->name('users.destroy');
